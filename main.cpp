ca3c465b78f5ba67d85fa0847f4d5c54605123d0
MrJac0bM <150305444+MrJac0bM@users.noreply.github.com>
2023-11-08 18:58:13 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,17 @@
+#include <iostream>
+#include "Bar.h"
 /*Mi proyecto va a crear tres clases orientadas a el manejor de un bar un ejemplo seria que tipos
  de coteles y se le dara un precio ademas de que igual los instrumentos(3 instrumentos) 
 se le dara un nombre y su precio para que al final pueda imprimir el tipo de coctel su costo y que herramientas se esta usando
 */
-#include <iostream>
-#include "Bar.h"
-using namespace std;
 
 int main() {
-  
     Coctel coctel1("Margarita", 7.50);
     Coctel coctel2("Cosmopolitan", 8.50);
     Coctel coctel3("Pina Colada", 6.50);
     Coctel coctel4("Daiquiri", 7.00);
     Coctel coctel5("Mojito", 8.00);
-  
+
     Coctelera coctelera1("Coctelera Mágica", 12.99);
     Coctelera coctelera2("Coctelera Fantástica", 11.99);
     Coctelera coctelera3("Coctelera de Ensueño", 14.99);
@@ -25,11 +23,11 @@ int main() {
     Colador colador3("Colador Profesional", 9.99);
     Colador colador4("Colador Rápido", 8.99);
     Colador colador5("Colador Deluxe", 10.99);
+    // Modificar atributos utilizando los métodos set
 
-    // Imprime información si quisiera imprimir un objeto diferente solo cambiar el numero "coctel"+num o coctelera+num
-    std::cout << "Coctel: " << coctel2.getNombre() << " - Precio: $" << coctel2.getPrecio() << std::endl;
-    std::cout << "Coctelera: " << coctelera2.getNombre() << " - Precio: $" << coctelera2.getPrecio() << std::endl;
-    std::cout << "Colador: " << colador2.getNombre() << " - Precio: $" << colador2.getPrecio() << std::endl;
-
+    // Llama a funciones adicionales de cada clase
+    coctel1.servir();
+    coctelera1.agitar();
+    colador1.colar();
     return 0;
 }

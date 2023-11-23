a8be24267bd296b0a4151ac1387c35ce0dcf2d61
MrJac0bM <150305444+MrJac0bM@users.noreply.github.com>
2023-11-22 22:27:05 -0600

    Update Cliente.h

--- Cliente.h
+++ Cliente.h
@@ -1,31 +1,13 @@
 #ifndef CLIENTE_H
 #define CLIENTE_H
 
-#include <string>
-#include <iostream>
-
-using namespace std;
+#include "Bebida.h"
 
 class Cliente {
-private:
-    string nombre;
-    int edad;
-    int id;
-
 public:
-    Cliente(string nombre, int edad, int id);
-    
-    string getNombre();
-    int getEdad();
-    int getId();
-
-    void setNombre(string nombre);
-    void setEdad(int edad);
-    void setId(int id);
-
-    void ordenarBebidaCaliente();
-    void ordenarBebidaFria();
-    void pagarCuenta();
+    void ordenar(Bebida& bebida) {
+        bebida.servir();
+    }
 };
 
 #endif

fda41a69f65e1ab3f2818f1dacf2d264b3c196b2
MrJac0bM <150305444+MrJac0bM@users.noreply.github.com>
2023-11-26 22:31:46 -0600

    Update Bartender.h

--- Bartender.h
+++ Bartender.h
@@ -7,11 +7,30 @@
 class Bartender {
 private:
     std::string nombre;
+    BebidaFria mojito;
+    BebidaFria margarita;
+    BebidaCaliente cafeIrlandes;
+    BebidaCaliente chocolateCaliente;
 public:
-    Bartender(std::string nombre) : nombre(nombre) {}
-    void prepararBebida(Cliente& cliente, Bebida& bebida) {
+    Bartender(std::string nombre) : nombre(nombre), mojito("Mojito", 5), margarita("Margarita", 5), cafeIrlandes("Café Irlandés", 5), chocolateCaliente("Chocolate Caliente", 5) {}
+    void prepararBebida(Cliente& cliente, int opcion) {
         std::cout << "El bartender " << nombre << " está preparando la bebida.\n";
-        cliente.ordenar(bebida);
+        switch (opcion) {
+            case 1:
+                cliente.ordenar(mojito);
+                break;
+            case 2:
+                cliente.ordenar(margarita);
+                break;
+            case 3:
+                cliente.ordenar(cafeIrlandes);
+                break;
+            case 4:
+                cliente.ordenar(chocolateCaliente);
+                break;
+            default:
+                std::cout << "Opción no válida.\n";
+        }
     }
 
     // Métodos get y set
@@ -20,4 +39,3 @@ public:
 };
 
 #endif
-

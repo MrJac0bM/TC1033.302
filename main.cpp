e831d0b57d50de1880753fb02d8e4cb82854de1b
MrJac0bM <150305444+MrJac0bM@users.noreply.github.com>
2023-11-26 22:28:55 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,42 +1,17 @@
-#ifndef COCTELERIA_H
-#define COCTELERIA_H
+#include <iostream>  
+#include "Cocteleria.h"  
 
-#include "Bartender.h"
-#include "Cliente.h"
-#include "Bebida.h"
+int main() { 
+    Cocteleria cocteleria;  
+    char opcion;  
+    do {  
+        cocteleria.abrirBar();  
+        std::cout << "¿Deseas ordenar otra bebida? (s/n): ";  
+        std::cin >> opcion;  
+    } while (opcion == 's' || opcion == 'S');  
 
-class Cocteleria {
-private:
-    Bartender bartender;
-    Cliente cliente;
-    BebidaFria mojito;
-    BebidaFria margarita;
-    BebidaCaliente cafeIrlandes;
-    BebidaCaliente chocolateCaliente;
-public:
-    Cocteleria() : bartender("Juan"), mojito("Mojito", 5), margarita("Margarita", 5), cafeIrlandes("Café Irlandés", 5), chocolateCaliente("Chocolate Caliente", 5) {}
-    void abrirBar() {
-        std::cout << "Bienvenido a la coctelería. ¿Qué tipo de bebida te gustaría? (1. Mojito, 2. Margarita, 3. Café Irlandés, 4. Chocolate Caliente): ";
-        int opcion;
-        std::cin >> opcion;
+    std::cout << "Gracias por visitar nuestra coctelería. ¡Vuelve pronto!\n";  
 
-        switch (opcion) {
-            case 1:
-                bartender.prepararBebida(cliente, mojito);
-                break;
-            case 2:
-                bartender.prepararBebida(cliente, margarita);
-                break;
-            case 3:
-                bartender.prepararBebida(cliente, cafeIrlandes);
-                break;
-            case 4:
-                bartender.prepararBebida(cliente, chocolateCaliente);
-                break;
-            default:
-                std::cout << "Opción no válida.\n";
-        }
-    }
-};
+    return 0;  
+}
 
-#endif

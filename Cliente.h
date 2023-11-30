555734ab88acdfa7c99c6e00c06a32804d5feb62
MrJac0bM <150305444+MrJac0bM@users.noreply.github.com>
2023-11-29 19:03:35 -0600

    Update Cliente.h

--- Cliente.h
+++ Cliente.h
@@ -3,11 +3,14 @@
 
 #include "Bebida.h"
 
+// Clase Cliente
 class Cliente {
 public:
+    // Método para ordenar una bebida
     void ordenar(Bebida& bebida) {
-        bebida.servir();
+        bebida.servir(); // Llama al método servir de la bebida
     }
 };
 
 #endif
+

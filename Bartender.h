73c7223b011604344143e884d07e5be6abd0912b
MrJac0bM <150305444+MrJac0bM@users.noreply.github.com>
2023-11-29 18:30:24 -0600

    Update Bartender.h

--- Bartender.h
+++ Bartender.h
@@ -1,20 +1,31 @@
+// Definición de macros de inclusión condicional para evitar conflictos de declaración
 #ifndef BARTENDER_H
 #define BARTENDER_H
 
+// Inclusión de encabezados necesarios
 #include "Cliente.h"
 #include "Bebida.h"
 
+// Definición de la clase Bartender
 class Bartender {
 private:
+    // Atributos de la clase Bartender
     std::string nombre;
     BebidaFria mojito;
     BebidaFria margarita;
     BebidaCaliente cafeIrlandes;
     BebidaCaliente chocolateCaliente;
+
 public:
+    // Constructor de la clase Bartender
     Bartender(std::string nombre) : nombre(nombre), mojito("Mojito", 5), margarita("Margarita", 5), cafeIrlandes("Café Irlandés", 5), chocolateCaliente("Chocolate Caliente", 5) {}
+
+    // Método para preparar una bebida para un cliente
     void prepararBebida(Cliente& cliente, int opcion) {
+        // Mensaje de preparación de bebida
         std::cout << "El bartender " << nombre << " está preparando la bebida.\n";
+
+        // Switch para determinar la opción seleccionada y ordenar la bebida correspondiente
         switch (opcion) {
             case 1:
                 cliente.ordenar(mojito);
@@ -29,13 +40,16 @@ public:
                 cliente.ordenar(chocolateCaliente);
                 break;
             default:
+                // Mensaje de opción no válida
                 std::cout << "Opción no válida.\n";
         }
     }
 
-    // Métodos get y set
+    // Métodos get y set para el atributo 'nombre'
     std::string getNombre() { return nombre; }
     void setNombre(std::string nuevoNombre) { nombre = nuevoNombre; }
 };
 
+// Fin de la definición condicional de macros
 #endif
+

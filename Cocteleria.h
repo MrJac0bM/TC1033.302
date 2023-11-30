a9ecec039f0ba6e38596d9ed6cd2ff6c21a02a55
MrJac0bM <150305444+MrJac0bM@users.noreply.github.com>
2023-11-29 19:04:39 -0600

    Update Cocteleria.h

--- Cocteleria.h
+++ Cocteleria.h
@@ -4,17 +4,25 @@
 #include "Bartender.h"
 #include "Cliente.h"
 
+// Clase Cocteleria
 class Cocteleria {
 private:
-    Bartender bartender;
-    Cliente cliente;
+    Bartender bartender; // Instancia de la clase Bartender
+    Cliente cliente;     // Instancia de la clase Cliente
 public:
+    // Constructor de la clase Cocteleria
     Cocteleria() : bartender("Juan") {}
+
+    // Método para abrir el bar y recibir pedidos
     void abrirBar() {
+        // Mostrar mensaje de bienvenida y opciones al cliente
         std::cout << "Bienvenido a la coctelería. ¿Qué tipo de bebida te gustaría? (1. Mojito, 2. Margarita, 3. Café Irlandés, 4. Chocolate Caliente): ";
+        
+        // Leer la opción del cliente desde la entrada estándar
         int opcion;
         std::cin >> opcion;
 
+        // Llamar al método prepararBebida del bartender para servir la bebida al cliente
         bartender.prepararBebida(cliente, opcion);
     }
 };
